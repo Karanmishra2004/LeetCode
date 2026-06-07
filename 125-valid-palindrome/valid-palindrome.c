9e747e79025fd684b8979c30586bc19a36fa4a6c
KARAN MISHRA <94131144+Karanmishra2004@users.noreply.github.com>
2026-06-07 18:58:52 +0530

    Time: 1 ms (31.00%) | Memory: 9.3 MB (20.49%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.c
@@ -0,0 +1,39 @@
+
+void to_lowercase(char *str) {
+    while (*str) {
+        *str = tolower((unsigned char)*str); // Cast to unsigned char for safety
+        str++;
+    }
+}
+
+void remove_non_alphanumeric(char *str) {
+    int i = 0; // Tracks the current read position
+    int j = 0; // Tracks the next write position
+
+    while (str[i] != '\0') {
+        // Check if the current character is a letter or a digit
+        if (isalnum((unsigned char)str[i])) {
+            str[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    
+    // Explicitly add the null terminator to mark the end of the new string
+    str[j] = '\0'; 
+}
+bool isPalindrome(char* s) {
+    remove_non_alphanumeric(s);
+    to_lowercase(s);
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        if (s[i] != s[len - i - 1])
+        {
+            return false;
+        }
+    
+    }
+    return true;
+
+    
+}
