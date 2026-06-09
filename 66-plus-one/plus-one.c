53d183f5f91663e3f5a2cc2fd5d4e9801a434c19
KARAN MISHRA <94131144+Karanmishra2004@users.noreply.github.com>
2026-06-09 14:46:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (32.70%) - LeetSync

--- 66-plus-one/plus-one.c
+++ 66-plus-one/plus-one.c
@@ -2,32 +2,27 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    for (int i = digitsSize - 1; i >= 0; i--) {
 
-    int *arr = (int*)malloc(digitsSize*sizeof(int));
-
-    for (int i = 0; i < digitsSize; i++) {
-        arr[i] = digits[i];
-    }
-for (int i = digitsSize - 1; i >= 0; i--) {
-        if (arr[i] < 9) {
-            arr[i]++;
+        if (digits[i] < 9) {
+            digits[i]++;
             *returnSize = digitsSize;
-            return arr;
+            return digits;
         }
 
-        arr[i] = 0;
+        digits[i] = 0;
     }
 
-    free(arr);
+    int* result = (int*)malloc((digitsSize + 1) * sizeof(int));
 
-    arr = (int*)malloc((digitsSize + 1) * sizeof(int));
-    arr[0] = 1;
+    result[0] = 1;
 
     for (int i = 1; i <= digitsSize; i++) {
-        arr[i] = 0;
+        result[i] = 0;
     }
 
     *returnSize = digitsSize + 1;
-    return arr;
+
+    return result;
     
 }
