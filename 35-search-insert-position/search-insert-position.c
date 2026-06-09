4edccc46cc8422b89c467de1b48b101c230c826b
KARAN MISHRA <94131144+Karanmishra2004@users.noreply.github.com>
2026-06-09 14:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.69%) - LeetSync

--- 35-search-insert-position/search-insert-position.c
+++ 35-search-insert-position/search-insert-position.c
@@ -6,10 +6,10 @@ int searchInsert(int* nums, int numsSize, int target) {
     while(left<= right)
     {
         int mid =left+(right-left)/2;
-        if(nums[mid] == target)
+        if(nums[mid] == target){
         return mid;
-
-        if(nums[mid] < target)
+        }
+        else if(nums[mid] < target)
         left = mid + 1;
         else
         right =mid-1;
