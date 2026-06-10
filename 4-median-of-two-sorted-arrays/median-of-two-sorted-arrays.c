b70ec2271143c5ea114277accaf7d2a15715783a
KARAN MISHRA <94131144+Karanmishra2004@users.noreply.github.com>
2026-06-10 23:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (13.77%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.c
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.c
@@ -3,20 +3,22 @@
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
 
     int total = nums1Size + nums2Size;
-    int *arr = (int*)malloc(total * sizeof(int));
-
-    int i = 0, j = 0, k = 0;
-
-    // Merge both arrays
-    while (i < nums1Size && j < nums2Size) {
-        if (nums1[i] < nums2[j]) {
-            arr[k++] = nums1[i++];
-        } else {
+    int *arr = (int*)malloc(total*sizeof(int));
+    int i =0;
+    int j =0 ;
+    int  k=0;
+    while(i < nums1Size && j < nums2Size)
+    {
+        if(nums1[i] > nums2[j])
+        {
             arr[k++] = nums2[j++];
+        }else
+        {
+            arr[k++] = nums1[i++];
         }
     }
 
-    // Remaining elements
+        // Remaining elements
     while (i < nums1Size) {
         arr[k++] = nums1[i++];
     }
